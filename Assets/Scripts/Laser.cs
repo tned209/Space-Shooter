0c084f6891c7461c2b45c4d789e4afc68b03bd65
tned209 <68984153+tned209@users.noreply.github.com>
2020-07-30 14:19:48 -0400

    Revert "Revert "Initial commit""
    This reverts commit 69fba2888dd4646c7d4b8c048c5d0907c8054fa8.

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private float _laserspeed = 15;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * _laserspeed * Time.deltaTime);
+        if (transform.position.y >= 8f)
+        {
+            if (transform.parent != null)
+            {
+                Destroy(transform.parent.gameObject);
+            }
+            Destroy(this.gameObject);
+        }
+    }
+}
